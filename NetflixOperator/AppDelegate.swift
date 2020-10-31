c52a6b5c083ee7a269322df741c3364a0d7b9fcb
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2020-10-31 15:56:25 +0100

    Initial Commit

---
+++ NetflixOperator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NetflixOperator
+//
+//  Created by Lukas Danckwerth on 31.10.20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NetflixOperator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NetflixOperator
+//
+//  Created by Lukas Danckwerth on 31.10.20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
